0dd45a373212e54f8ee7e2151b09f667981559f0
mac <JOE@macdeMac-mini.local>
2017-05-17 16:31:09 +0800

    1.创建ZZJImageZoomView类 2.创建项目

---
+++ ZZJImageZoomView/ZZJImageZoomView/ZZJImageZoomView/ZZJImageZoomView.h
@@ -0,0 +1,13 @@
+//
+//  ZZJImageZoomView.h
+//  ZZJImageZoomView
+//
+//  Created by JOE on 2017/5/17.
+//  Copyright © 2017年 ZZJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZZJImageZoomView : UIView
+
+@end
---
+++ ZZJImageZoomView/ZZJImageZoomView/ZZJImageZoomView/ZZJImageZoomView.m
@@ -0,0 +1,21 @@
+//
+//  ZZJImageZoomView.m
+//  ZZJImageZoomView
+//
+//  Created by JOE on 2017/5/17.
+//  Copyright © 2017年 ZZJ. All rights reserved.
+//
+
+#import "ZZJImageZoomView.h"
+
+@implementation ZZJImageZoomView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
